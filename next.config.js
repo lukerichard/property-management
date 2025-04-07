d56512433257801f0b47af9b73781f28d597768c
Luke Richard <lukerichard@gmail.com>
2025-04-07 12:06:36 -0400

    Update next.config.js for Netlify deployment and add netlify.toml

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   poweredByHeader: false,
   compress: true,
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig; 
